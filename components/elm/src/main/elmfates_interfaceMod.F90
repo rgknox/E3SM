7cfecfa98d0b6bc79bf3dff8a07c7081a18778d9
Ryan Knox <rgknox@lbl.gov>
2022-08-03 12:34:29 -0400

    Adding canopy layer history diagnostics for fates

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -2426,7 +2426,7 @@ end subroutine wrap_update_hifrq_hist
    use FatesIOVariableKindMod, only : site_fuel_r8, site_cwdsc_r8, site_scag_r8
    use FatesIOVariableKindMod, only : site_scagpft_r8, site_agepft_r8, site_agefuel_r8
    use FatesIOVariableKindMod, only : site_height_r8, site_elem_r8, site_elpft_r8
-   use FatesIOVariableKindMod, only : site_elcwd_r8, site_elage_r8
+   use FatesIOVariableKindMod, only : site_elcwd_r8, site_elage_r8, site_clscpf_r8
    use FatesIOVariableKindMod, only : site_coage_r8, site_coage_pft_r8
    use FatesIOVariableKindMod, only : site_can_r8, site_cnlf_r8, site_cnlfpft_r8
    use FatesIODimensionsMod, only : fates_bounds_type
@@ -2542,7 +2542,7 @@ end subroutine wrap_update_hifrq_hist
              site_can_r8,site_cnlf_r8, site_cnlfpft_r8, site_scag_r8, &
              site_scagpft_r8, site_agepft_r8, site_elem_r8, site_elpft_r8, &
              site_elcwd_r8, site_elage_r8, site_coage_r8, site_coage_pft_r8, &
-             site_agefuel_r8)
+             site_agefuel_r8, site_clscpf_r8)
 
            d_index = fates_hist%dim_kinds(dk_index)%dim2_index
            dim2name = fates_hist%dim_bounds(d_index)%name
@@ -2887,6 +2887,9 @@ end subroutine wrap_update_hifrq_hist
    fates%agefuel_begin = 1
    fates%agefuel_end   = nlevage_fates * nfsc_fates
 
+   fates%clscpf_begin = 1
+   fates%clscpf_end = numpft_fates*nlevsclass_fates*nclmax_fates
+   
  end subroutine hlm_bounds_to_fates_bounds
 
 ! ======================================================================================
