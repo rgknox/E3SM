7cdde26a411a272fd3e647c8b18fe82dbbc2e0eb
Ryan Knox <rgknox@lbl.gov>
2026-04-23 12:49:37 -0400

    support for fates dynamic vmax

--- components/elm/src/main/elmfates_interfaceMod.F90
+++ components/elm/src/main/elmfates_interfaceMod.F90
@@ -3448,6 +3448,7 @@ end subroutine wrap_update_hifrq_hist
    use FatesIOVariableKindMod, only : site_can_r8, site_cnlf_r8, site_cnlfpft_r8
    use FatesIOVariableKindMod, only : site_cdpf_r8, site_cdsc_r8, site_cdam_r8
    use FatesIOVariableKindMod, only : site_landuse_r8, site_lulu_r8, site_lupft_r8
+   use FatesIOVariableKindMod, only : site_clpf_r8
    use FatesIODimensionsMod, only : fates_bounds_type
 
 
@@ -3549,7 +3550,7 @@ end subroutine wrap_update_hifrq_hist
              site_scagpft_r8, site_agepft_r8, site_elem_r8, site_elpft_r8, &
              site_elcwd_r8, site_elage_r8, site_coage_r8, site_coage_pft_r8, &
              site_agefuel_r8,site_cdsc_r8, site_cdpf_r8, site_cdam_r8, &
-             site_landuse_r8, site_lulu_r8, site_lupft_r8)
+             site_landuse_r8, site_lulu_r8, site_lupft_r8, site_clpf_r8)
 
            d_index = fates_hist%dim_kinds(dk_index)%dim2_index
            dim2name = fates_hist%dim_bounds(d_index)%name
@@ -3854,6 +3855,9 @@ end subroutine wrap_update_hifrq_hist
    fates%cdpf_begin = 1
    fates%cdpf_end = nlevdamage_fates * numpft_fates * nlevsclass_fates
 
+   fates%clpf_begin = 1
+   fates%clpf_end   = numpft_fates * nclmax_fates
+   
    fates%cdsc_begin = 1
    fates%cdsc_end = nlevdamage_fates * nlevsclass_fates
    
